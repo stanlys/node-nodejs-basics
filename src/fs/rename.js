247370a198005decff3ad2b5281bf595ef66d85d
Stanlys <stanlys@yandex.ru>
2023-05-24 17:05:29 +0300

    feat: rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { copyFile, constants } from "node:fs/promises";
+import { existsSync } from "node:fs";
+
+const BASE = "./src/fs/files/";
+
 const rename = async () => {
-    // Write your code here 
+    if (!existsSync(`${BASE}wrongFilename.txt`)) {
+        throw new Error("FS operation failed");
+    }
+    try {
+        await copyFile(`${BASE}wrongFilename.txt`, `${BASE}properFilename.md`, constants.COPYFILE_EXCL);
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
 
 await rename();
